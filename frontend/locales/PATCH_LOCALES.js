2994a10a66009b42605d037b6c4b6b7f64e783c1
PhilippePerret <philippe.perret@yahoo.fr>
2026-08-08 07:23:21 +0200

    Patch pour ajouter des locales

---
+++ frontend/locales/PATCH_LOCALES.js
@@ -0,0 +1,20 @@
+/**
+ * Patch locales
+ * 
+ * On met dans ce fichier toutes les nouvelles locales qui ne sont pas
+ * encore traduites et qui devront l'être.
+ */
+Object.assign(MESSAGES, {
+
+  // Nouveaux messages à mettre dans les MESSAGES.js
+
+  // Git
+  'git-issue-gestion': "Gestion des issues Github"
+
+})
+
+Object.assign.ERRORS, {
+
+  // Nouveaus messages d'erreur à mettre dans les ERRORS.js
+
+}
